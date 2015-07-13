17739786d7f592df3c47dc9124884857784cf185
royfdo <roy@exilesoft.com>
2015-07-13 14:42:06 +0530

    Added class comments

--- src/java8/lambda/expression/MathService.java
+++ src/java8/lambda/expression/MathService.java
@@ -1,8 +1,13 @@
 package java8.lambda.expression;
 
+/**
+ * Math service functional interface.
+ * 
+ * @author roy
+ */
 @FunctionalInterface
 public interface MathService {
-	
+
 	int operation(int a, int b);
 
 }
